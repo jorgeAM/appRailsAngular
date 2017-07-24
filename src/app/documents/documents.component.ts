710b0e1ac2d464160b1fd0144609168fcecfff4c
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-07-24 00:08:12 -0500

    data mock, solo para probar

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
 
+//importamos nuesta interface
+import { Documents } from './documents';
+
 @Component({
   selector: 'documents',
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent {
+	documents: Documents[] = [
+		{
+			title: "My first Doc",
+			description: "Agg tmr!!",
+			file_url: "http://google.com",
+			update_at: "24/07/16",
+			image_url: "http://google.com"
+		},
+		{
+			title: "My second Doc",
+			description: "Alto ahi Crrano!",
+			file_url: "http://google.com",
+			update_at: "24/07/16",
+			image_url: "http://google.com"
+		},
+		{
+			title: "My last Doc",
+			description: "Agg csm!!",
+			file_url: "http://google.com",
+			update_at: "24/07/16",
+			image_url: "http://google.com"
+		}
+	];
 }

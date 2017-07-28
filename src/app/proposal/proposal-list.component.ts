e86e268858972de55280edf8c448ff8d59f87ba9
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-07-28 11:47:58 -0500

    usamos la clase proposal y la mostramos en el template

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   selector: 'proposal-list',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
 })
 
 export class ProposalListComponent {
-	
+	proposalOne: Proposal = new Proposal(
+		15, 'Abc Company', 'http://google.com', 'RoR', 150, 120, 15, 'jorge.alfmur@gmail.com');
+	proposalTwo: Proposal = new Proposal(
+		99, 'Xyz Company', 'http://facebook.com', 'RoR', 150, 120, 15, 'jorge.alfmur@hotmail.com');
+	proposalThree: Proposal = new Proposal(
+		300, '123 Company', 'http://twitter.com', 'RoR', 150, 120, 15, 'jorge.alfmur@outlook.com');
+
+	proposals: Proposal[] = [
+		this.proposalOne, this.proposalTwo, this.proposalThree
+	];
 }

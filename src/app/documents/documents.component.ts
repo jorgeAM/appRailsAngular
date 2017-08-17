cb7824964bf002ca777a7cde3fd745d1fd9f4e74
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-17 00:39:37 -0500

    REVISAR:COMUNICAMOS API CON ANGULAR

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,35 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+//observable
+import { Observable } from 'rxjs/Rx';
 //importamos nuesta interface
 import { Documents } from './documents';
+//importamos el servicio de documents
+import { DocumentsService } from './documents.service';
 
 @Component({
   selector: 'documents',
   templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.css']
+  styleUrls: ['./documents.component.css'],
+  providers: [
+  	DocumentsService
+  ]
 })
-export class DocumentsComponent {
-	documents: Documents[] = [
-		{
-			title: "My first Doc",
-			description: "Agg tmr!!",
-			file_url: "http://google.com",
-			update_at: "24/07/16",
-			image_url: "http://google.com"
-		},
-		{
-			title: "My second Doc",
-			description: "Alto ahi Crrano!",
-			file_url: "http://google.com",
-			update_at: "24/07/16",
-			image_url: "http://google.com"
-		},
-		{
-			title: "My last Doc",
-			description: "Agg csm!!",
-			file_url: "http://google.com",
-			update_at: "24/07/16",
-			image_url: "http://google.com"
-		}
-	];
+export class DocumentsComponent implements OnInit {
+	documents: Documents[];
+	errorMessage: string;
+	mode = "Observable";
+
+	constructor(private documentsService: DocumentsService){}
+	
+	ngOnInit(){
+		let timer = Observable.timer(0, 5000);
+		timer.subscribe(()=> this.getDocuments());
+	}
+
+	getDocuments(){
+		this.documentsService.getDocuments()
+			.subscribe(
+				documents => this.documents = documents,
+				error => this.errorMessage = <any>error
+			);
+	}
 }

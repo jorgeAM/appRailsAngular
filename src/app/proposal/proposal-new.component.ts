7b0f33cf8bf498f3443c6b87c532385dd5829697
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-20 15:38:15 -0500

    configuraos app en angular para que podamos agregar proposal

--- src/app/proposal/proposal-new.component.ts
+++ src/app/proposal/proposal-new.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+//servicies de proposal
+import { ProposalService } from './proposal.service';
 
 @Component({
   selector: 'proposal-new',
   templateUrl: './proposal-new.component.html',
-  styleUrls: ['./proposal-new.component.css']
+  styleUrls: ['./proposal-new.component.css'],
+  providers: [ProposalService]
 })
 
 export class ProposalNewComponent {
-	proposal: Proposal = new Proposal;
+	proposal = new Proposal;
+	submitted: boolean = false;
+
+	constructor(
+		private proposalService: ProposalService
+	){}
+
+	createProposal(proposal) {
+		this.submitted = true;
+		this.proposalService.createProposal(proposal)
+			.subscribe(
+				data => { return true },
+				error => {
+					console.log("Error saving proposal");
+					return Observable.throw(error);
+				}
+			);
+	}
 }
--- src/app/proposal/proposal.service.ts
+++ src/app/proposal/proposal.service.ts
@@ -25,6 +25,14 @@ export class ProposalService {
 		return this.http.get(this.proposalUrl + '/' + id);
 	}
 
+	createProposal(proposal) {
+	    let headers = new Headers({ 'Content-Type': 'application/json'});
+	    let options = new RequestOptions({ headers: headers });
+	    return this.http
+	    	.post(this.proposalUrl, JSON.stringify(proposal), { headers: headers})
+	    		.map((res: Response) => res.json());
+	}
+
 	private handleError (error: Response | any){
     	// In a real world app, we might use a remote logging infrastructure
 	    let errMsg: string;
